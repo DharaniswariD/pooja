2c0d176ca04c085c6225dc11e371be7e3e54fc11
DharaniswariD <52819416+DharaniswariD@users.noreply.github.com>
2019-07-12 15:49:24 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,4 +1,4 @@
-ch=(input(""))
+ch=input()
 if(ch == "a" or ch == "e" or ch == "i" or ch == "o" or ch == "u" or ch == "A" or ch == "E" or ch == "I" or ch == "O" or ch == "U"):
 print ("is a vowel")
 else:

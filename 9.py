20e763ae4a77fee06b9fef315e8184979c914842
DharaniswariD <52819416+DharaniswariD@users.noreply.github.com>
2019-07-13 16:19:35 +0530

    Update 9.py

--- 9.py
+++ 9.py
@@ -1,8 +1,8 @@
-N,K=input().split()
-N=int(N)
-K=int(K)
-s=list(map(int,input().strip().split()))
-sum=0
+N,K = input().split()
+N = int(N)
+K = int(K)
+s = list(map(int,input().strip().split()))
+sum = 0
 for i in range(1,k+1):
-  sum=sum+i
+  sum = sum + i
 print(sum)

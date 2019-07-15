7dad4c6e1f4e7ffd44564bcacf43b4a5183e5326
DharaniswariD <52819416+DharaniswariD@users.noreply.github.com>
2019-07-15 11:30:11 +0530

    Update 13.py

--- 13.py
+++ 13.py
@@ -1,9 +1,9 @@
 num=int(input())
 if(num>1):
-  for i in range(2,num):
-  if(num%i==0):
-    print("no")
-    break
-  else :
-    print("yes")
+for i in range(2,num):
+if(num%i==0):
+  print("no")
+  break
+else :
+  print("yes")
     

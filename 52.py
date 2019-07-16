01ca7218d998618a541de09fe6c8ab965ddfad18
DharaniswariD <52819416+DharaniswariD@users.noreply.github.com>
2019-07-16 16:10:44 +0530

    Create 52.py

---
+++ 52.py
@@ -0,0 +1,12 @@
+p,q=map(int,input().split())
+r,s=map(int,input().split())
+t,u=map(int,input().split())
+v,w=map(int,input().split())
+m=d-b
+n=f-h
+o=e-c
+s=g-a
+if m==n==o==s:
+    print("yes")
+else:
+    print("no")
